6448b07eb07439de1f1efcca1ca5b21352df98c1
lubelle <belleliullh@gmail.com>
2018-12-31 00:21:26 -0500

    add answer component

--- src/app/qa-answer/qa-answer.component.ts
+++ src/app/qa-answer/qa-answer.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { Answer } from './../interfaces';
 
 @Component({
   selector: 'app-qa-answer',
   templateUrl: './qa-answer.component.html',
   styleUrls: ['./qa-answer.component.css']
 })
-export class QaAnswerComponent implements OnInit {
-
+export class QaAnswerComponent {
+  @Input() answer: Answer;
+  @Input() index: number;
+  @Output() deleteEvent = new EventEmitter<Answer>();
   constructor() { }
 
-  ngOnInit() {
+  requestDelete() {
+    console.log('AnswerComponent requestDelete()');
+    this.deleteEvent.emit(this.answer);
   }
 
 }
