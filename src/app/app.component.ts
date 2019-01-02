818749685044c0729c9c27d25ed46febc5aa02c1
lubelle <belleliullh@gmail.com>
2019-01-01 22:58:05 -0500

    use *ngFor to loop thru answers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
+import { QaAnswerComponent } from './qa-answer/qa-answer.component';
+import { QaHeaderComponent } from './qa-header/qa-header.component';
 import { QuestionAnswers, Answer } from './interfaces';
 import { Component } from '@angular/core';
+import { headersToString } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<app-qa-header [myTitle]="appTitle"></app-qa-header>
+             <app-qa-question [question]="questionAnswers.question"></app-qa-question>
+             <app-qa-answer *ngFor="let ans of questionAnswers.answers; let i = index"
+             [answer]="ans" [index]="i+1" (deleteEvent)="deleteAnswer($event)"></app-qa-answer>
+             `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
@@ -12,7 +19,8 @@ export class AppComponent {
     question: { author: 'John', content: 'The meaning of life?' },
     answers: [
       { author: 'Brown', content: `The answer is number 42.`, accepted: true},
-      { author: 'Jane', content: `The answer is right infront of you.`}
+      { author: 'Jane', content: `The answer is right infront of you.`},
+      { author: 'Ben', content: `No clue`}
     ]
 
   };
